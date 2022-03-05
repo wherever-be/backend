07093250cb7d328dcf9aa3db23d47d279330d378
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-03-05 22:07:35 +0100

    refactor: simplify & modularify ryanair scraper

--- chalicelib/scraper/ryanair.py
+++ chalicelib/scraper/ryanair.py
@@ -1,59 +1,21 @@
 import requests
-from typing import List
-from datetime import datetime
-
-from chalicelib.connection import Connection
-from chalicelib.price import Price
-from .errors import AirportsNotConnectedError, TooManyRequestsError, UnknownError
-from .search_parameters import SearchParameters
-
-
-def search(search_parameters: SearchParameters) -> List[Connection]:
-    try:
-        response = make_request(search_parameters)
-    except AirportsNotConnectedError:
-        return []
-    flights = response["trips"][0]["dates"][0]["flights"]
-    return [
-        Connection(
-            from_airport=search_parameters.origin_iata,
-            departure=datetime.fromisoformat(flight["segments"][0]["time"][0]),
-            to_airport=search_parameters.destination_iata,
-            arrival=datetime.fromisoformat(flight["segments"][-1]["time"][-1]),
-            price=Price(
-                amount=flight["regularFare"]["fares"][0]["amount"],
-                currency=response["currency"],
-            ),
-        )
-        for flight in flights
-    ]
 
 
 # TODO: retry on some errors, throttle
-def make_request(search_parameters: SearchParameters):
-    request = requests.get(
-        "https://www.ryanair.com/api/booking/v4/en-gb/availability",
-        params=dict(
-            ADT=search_parameters.num_people,  # number of adults
-            TEEN=0,  # number of 12-15 year olds
-            CHD=0,  # number of 2-11 year olds
-            INF=0,  # number of <2 year olds
-            DateIn="",  # empty for one-way
-            DateOut=search_parameters.flight_date.isoformat(),
-            Origin=search_parameters.origin_iata,
-            Destination=search_parameters.destination_iata,
-            Disc=0,  # no idea what this is
-            promoCode="",
-            IncludeConnectingFlights=False,
-            FlexDaysBeforeOut=0,
-            FlexDaysOut=0,  # TODO: this can be up to 6, then we get results for later dates as well
-            ToUs="AGREED",
-        ),
-    )
+def make_request(url: str, parameters):
+    request = requests.get(url, params=parameters)
     if request.status_code == 429:
         raise TooManyRequestsError()
-    if request.status_code == 404:
-        raise AirportsNotConnectedError()
     if request.status_code != 200:
-        raise UnknownError()
+        raise RyanairAPIError(request.status_code)
     return request.json()
+
+
+class TooManyRequestsError(Exception):
+    pass
+
+
+class RyanairAPIError(Exception):
+    def __init__(self, code: int):
+        super().__init__()
+        self.code = code
---
+++ chalicelib/scraper/search_flights.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from datetime import date, datetime
+from typing import List
+
+from chalicelib.connection import Connection
+from chalicelib.price import Price
+from .ryanair import make_request, RyanairAPIError
+
+
+def search_flights(
+    num_people: int, flight_date: date, origin_iata: str, destination_iata: str
+) -> List[Connection]:
+    try:
+        response = make_request(
+            "https://www.ryanair.com/api/booking/v4/en-gb/availability",
+            parameters=dict(
+                ADT=num_people,  # number of adults
+                TEEN=0,  # number of 12-15 year olds
+                CHD=0,  # number of 2-11 year olds
+                INF=0,  # number of <2 year olds
+                DateIn="",  # empty for one-way
+                DateOut=flight_date.isoformat(),
+                Origin=origin_iata,
+                Destination=destination_iata,
+                Disc=0,  # no idea what this is
+                promoCode="",
+                IncludeConnectingFlights=False,
+                FlexDaysBeforeOut=0,
+                FlexDaysOut=0,  # TODO: this can be up to 6, then we get results for later dates as well
+                ToUs="AGREED",
+            ),
+        )
+    except RyanairAPIError as api_error:
+        if api_error.code == 404:
+            return []  # no connecting flights
+        raise api_error
+    flights = response["trips"][0]["dates"][0]["flights"]
+    return [
+        Connection(
+            from_airport=origin_iata,
+            departure=datetime.fromisoformat(flight["segments"][0]["time"][0]),
+            to_airport=destination_iata,
+            arrival=datetime.fromisoformat(flight["segments"][-1]["time"][-1]),
+            price=Price(
+                amount=flight["regularFare"]["fares"][0]["amount"],
+                currency=response["currency"],
+            ),
+        )
+        for flight in flights
+    ]
