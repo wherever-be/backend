da35fb2a0c666aa6dd37205f78f30ca329a8dc3a
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-03-05 16:07:42 +0100

    feature: mock api

---
+++ app.py
@@ -0,0 +1,54 @@
+from chalice import Chalice
+import time
+import random
+
+
+app = Chalice(app_name="backend")
+
+
+@app.route("/", methods=["POST"])
+def index():
+    request_data = app.current_request.json_body
+    time.sleep(1)
+    return {
+        "searchResults": [
+            {
+                "id": "0xdeadbeef",
+                "destination": random.choice(["Brussels", "Berlin"]),
+                "goodness": random.uniform(-100, 100),
+                "journeys": [
+                    {
+                        "friendName": random.choice(
+                            ["Joshua", "Chris", "Jennifer", "Caroline"]
+                        ),
+                        "staysHome": False,
+                        "homeToDest": {
+                            "departure": {
+                                "date": "2015-12-12T13:52:00.000Z",
+                                "port": "ARN",
+                            },
+                            "arrival": {
+                                "date": "2015-12-12T16:31:00.000Z",
+                                "port": "BRU",
+                            },
+                            "price": {"amount": random.uniform(1, 100), "unit": "EUR"},
+                            "bookingLink": "http://www.ryanair.com/book/coolflights.php",
+                        },
+                        "destToHome": {
+                            "departure": {
+                                "date": "2015-12-17T23:11:00.000Z",
+                                "port": "BRU",
+                            },
+                            "arrival": {
+                                "date": "2015-12-18T02:21:00.000Z",
+                                "port": "NYO",
+                            },
+                            "price": {"amount": random.uniform(1, 100), "unit": "EUR"},
+                            "bookingLink": "http://www.wizzair.com/book/otherflights.cgi",
+                        },
+                    }
+                ],
+            }
+            for _ in range(5)
+        ]
+    }
