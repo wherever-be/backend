a8ce1cb9a1ce461221d76e2deb5004851dfe4cae
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-03-06 20:55:04 +0100

    improve: introduce rate limits to ryanair scraper

--- chalicelib/scraper/ryanair.py
+++ chalicelib/scraper/ryanair.py
@@ -1,14 +1,13 @@
+from ratelimit import limits, RateLimitException
 import requests
-
-
-# TODO: retry on some errors, throttle
-def make_request(url: str, parameters):
-    request = requests.get(url, params=parameters)
-    if request.status_code == 429:
-        raise TooManyRequestsError()
-    if request.status_code != 200:
-        raise RyanairAPIError(request.status_code)
-    return request.json()
+from tenacity import (
+    retry,
+    retry_if_exception_type,
+    wait_exponential,
+    wait_fixed,
+    RetryCallState,
+    wait_fixed,
+)
 
 
 class TooManyRequestsError(Exception):
@@ -19,3 +18,26 @@ class RyanairAPIError(Exception):
     def __init__(self, code: int):
         super().__init__()
         self.code = code
+
+
+def warn_on_exception(retry_state: RetryCallState):
+    print(f"{retry_state.fn.__name__} produced {retry_state.outcome.exception()}")
+
+
+@retry(
+    retry=retry_if_exception_type(TooManyRequestsError),
+    wait=wait_exponential(),
+    before_sleep=warn_on_exception,
+)
+@retry(
+    retry=retry_if_exception_type(RateLimitException),
+    wait=wait_fixed(1),
+)
+@limits(calls=1, period=1)
+def make_request(url: str, parameters):
+    request = requests.get(url, params=parameters)
+    if request.status_code == 429:
+        raise TooManyRequestsError()
+    if request.status_code != 200:
+        raise RyanairAPIError(request.status_code)
+    return request.json()
