f16a466edf3b451d39c8c1ad83219a1b8415c01e
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-03-06 23:22:43 +0100

    test: simple search

--- test/test_request.py
+++ test/test_request.py
@@ -1,6 +1,9 @@
-from datetime import date
+from datetime import date, timedelta
 
 from chalicelib import Request
+from chalicelib.time_frame import TimeFrame
+from chalicelib.friend import Friend
+from chalicelib.scraper import world
 
 
 def test_from_frontend_json():
@@ -23,3 +26,23 @@ def test_from_frontend_json():
     assert len(request.friends) == 3
     assert request.destination_country.code == "fr"
     assert request.destination_city.code == "PARIS"
+
+
+def test_simple_search():
+    request = Request(
+        time_frame=TimeFrame(
+            start_date=date.today(), end_date=date.today() + timedelta(days=14)
+        ),
+        min_days=3,
+        max_days=7,
+        friends=[
+            Friend(
+                name="Jake",
+                city=world().city_by_code("STOCKHOLM"),
+            )
+        ],
+        destination_country=world().country_by_code("it"),
+        destination_city=world().city_by_code("ROME"),
+    )
+    response = request.response
+    assert len(response.trips) >= 1
