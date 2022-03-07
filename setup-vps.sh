904f409998541925182e96fe8dd48132c2f0ef26
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-03-07 21:17:07 +0100

    chore: assume that repo is cloned in vps setup script

--- setup-vps.sh
+++ setup-vps.sh
@@ -5,6 +5,4 @@ sudo hostname backend
 sudo apt install python3-pip -y
 pip3 install poetry
 poetry config virtualenvs.in-project true
-git clone https://github.com/wherever-be/backend.git
-cd backend
 poetry install
