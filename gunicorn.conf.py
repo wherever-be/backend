85b2b024b75ac6c89dfe838de2223caf7f0b682e
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-03-25 21:49:07 +0100

    chore: change port

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 wsgi_app = "server:app"
-bind = ["0.0.0.0:443"]
+bind = ["0.0.0.0:42069"]
 keyfile = "/etc/letsencrypt/live/wherever.be/privkey.pem"
 certfile = "/etc/letsencrypt/live/wherever.be/fullchain.pem"
 accesslog = "./log.txt"
