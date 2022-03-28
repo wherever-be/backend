228a34f2da5298cd6ab0208e6d584a32897533a1
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-03-28 15:50:32 +0200

    feature: slow down polling to limit cpu usage

--- backend/caching/poll.py
+++ backend/caching/poll.py
@@ -1,8 +1,7 @@
 from datetime import date, timedelta
 from itertools import combinations
 import random
-from ratelimit import limits, RateLimitException
-from tenacity import retry, retry_if_exception_type, wait_fixed, wait_fixed
+from ratelimit import limits, sleep_and_retry
 from tqdm import tqdm
 from threading import Thread
 
@@ -23,34 +22,37 @@ def background_poll_loop():
 def poll_loop():
     all_caches.load_all()
     while True:
-        poll()
+        poll_all()
         all_caches.clean_all()
         all_caches.save_all()
 
 
-@retry(
-    retry=retry_if_exception_type(RateLimitException),
-    wait=wait_fixed(60),
-)
+@sleep_and_retry
 @limits(calls=1, period=60 * 60)
-def poll():
+def poll_all():
     cities = list(world().cities)
     city_pairs = list(combinations(cities, 2))
     loading_bar = tqdm(city_pairs, desc="Refreshing cache", smoothing=0)
     for from_city, to_city in loading_bar:
         loading_bar.set_postfix_str(f"{from_city.name} => {to_city.name}")
-        start_date = date.today() + timedelta(days=2 ** random.randint(0, 4))
-        end_date = start_date + timedelta(days=2 ** random.randint(3, 8))
-        min_days = random.randint(2, 5)
-        max_days = min_days + random.randint(1, 10)
-        query = Query(
-            time_frame=TimeFrame(start_date=start_date, end_date=end_date),
-            min_days=min_days,
-            max_days=max_days,
-            friends=[Friend(name="CACHE REFRESH", city=from_city)],
-            destination_country=next(
-                country for country in world().countries if to_city in country.cities
-            ),
-            destination_city=to_city,
-        )
-        search(query)
+        poll_single(from_city=from_city, to_city=to_city)
+
+
+@sleep_and_retry
+@limits(calls=1, period=0.25)
+def poll_single(from_city, to_city):
+    start_date = date.today() + timedelta(days=2 ** random.randint(0, 4))
+    end_date = start_date + timedelta(days=2 ** random.randint(3, 8))
+    min_days = random.randint(2, 5)
+    max_days = min_days + random.randint(1, 10)
+    query = Query(
+        time_frame=TimeFrame(start_date=start_date, end_date=end_date),
+        min_days=min_days,
+        max_days=max_days,
+        friends=[Friend(name="CACHE REFRESH", city=from_city)],
+        destination_country=next(
+            country for country in world().countries if to_city in country.cities
+        ),
+        destination_city=to_city,
+    )
+    search(query)
---
+++ test/caching/test_poll.py
@@ -0,0 +1,18 @@
+from time import time
+
+from backend.apis import world
+from backend.caching.poll import poll_single
+
+
+def test_rate_limit():
+    olsztyn = world().city_by_code("OLSZTYN")
+    brno = world().city_by_code("BRNO")
+    # run once to make cache
+    poll_single(from_city=olsztyn, to_city=brno)
+
+    start_time = time()
+    poll_single(from_city=olsztyn, to_city=brno)
+    assert time() - start_time < 0.01
+
+    poll_single(from_city=olsztyn, to_city=brno)
+    assert time() - start_time > 0.25
