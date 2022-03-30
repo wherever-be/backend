b7cd670d61f70301cd294ac40b7d60248ba4a291
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-03-30 13:42:03 +0200

    test: update correct list of connected airports for mmx

--- test/apis/ryanair/test_connected_airports.py
+++ test/apis/ryanair/test_connected_airports.py
@@ -4,4 +4,4 @@ from backend.apis.ryanair import connected_airports, world
 def test_malmo():
     airport = world().airport_by_iata("MMX")
     others = connected_airports(airport)
-    assert {other.iata for other in others} == {"ZAG", "KRK", "ARN"}
+    assert {other.iata for other in others} == {"ZAG", "ARN"}
