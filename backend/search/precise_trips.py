750497dbc67ac33cec9e6c8de54510e17fd37f43
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-04-01 23:11:19 +0200

    improve: realize faster if ryanair is blocking us

--- backend/search/precise_trips.py
+++ backend/search/precise_trips.py
@@ -7,39 +7,41 @@ from .rough_trips import rough_trips
 
 
 def precise_trips(query: Query, max_trips: int):
-    expanded = [
-        combined
-        for rough_trip in rough_trips(query, max_trips=max_trips)
-        for combined in Trip.combine_journeys(
-            destination=rough_trip.destination,
-            journeys=[
-                precise_journeys(rough_journey) for rough_journey in rough_trip.journeys
-            ],
-        )
-    ]
-    return pick_varied(candidates=expanded, max_trips=max_trips)
-
-
-def precise_journeys(rough_journey: Journey):
+    _rough_trips = rough_trips(query, max_trips=max_trips)
     try:
-        return [
-            Journey(
-                friend=rough_journey.friend,
-                home_to_destination=home_to_destination,
-                destination_to_home=destination_to_home,
-            )
-            for home_to_destination in precise_connections(
-                num_people=1,  # TODO: group people
-                flight_date=rough_journey.home_to_destination.departure.date(),
-                origin=rough_journey.home_to_destination.from_airport,
-                destination=rough_journey.home_to_destination.to_airport,
-            )
-            for destination_to_home in precise_connections(
-                num_people=1,  # TODO: group people
-                flight_date=rough_journey.destination_to_home.departure.date(),
-                origin=rough_journey.destination_to_home.from_airport,
-                destination=rough_journey.destination_to_home.to_airport,
+        _precise_trips = [
+            combined
+            for rough_trip in _rough_trips
+            for combined in Trip.combine_journeys(
+                destination=rough_trip.destination,
+                journeys=[
+                    precise_journeys(rough_journey)
+                    for rough_journey in rough_trip.journeys
+                ],
             )
         ]
     except RyanairBlacklistError:
-        return [rough_journey]
+        return _rough_trips
+    return pick_varied(candidates=_precise_trips, max_trips=max_trips)
+
+
+def precise_journeys(rough_journey: Journey):
+    return [
+        Journey(
+            friend=rough_journey.friend,
+            home_to_destination=home_to_destination,
+            destination_to_home=destination_to_home,
+        )
+        for home_to_destination in precise_connections(
+            num_people=1,  # TODO: group people
+            flight_date=rough_journey.home_to_destination.departure.date(),
+            origin=rough_journey.home_to_destination.from_airport,
+            destination=rough_journey.home_to_destination.to_airport,
+        )
+        for destination_to_home in precise_connections(
+            num_people=1,  # TODO: group people
+            flight_date=rough_journey.destination_to_home.departure.date(),
+            origin=rough_journey.destination_to_home.from_airport,
+            destination=rough_journey.destination_to_home.to_airport,
+        )
+    ]
